b8b8fff18013a9f1edae67ef98f6aac82f15fa3f
oalam <bailet.thomas@gmail.com>
2020-01-30 14:27:43 +0100

    add loader

---
+++ loader/src/main/scala/com/hurence/historian/App.scala
@@ -0,0 +1,313 @@
+package com.hurence.historian
+
+
+import com.hurence.historian.LoaderMode.LoaderMode
+import com.hurence.logisland.processor.StandardProcessContext
+import com.hurence.logisland.record.{FieldDictionary, Record, RecordDictionary, StandardRecord, TimeSeriesRecord}
+import com.hurence.logisland.timeseries.converter.compaction.BinaryCompactionConverter
+import org.apache.commons.cli.{CommandLine, GnuParser, Option, OptionBuilder, Options, Parser}
+import org.apache.spark.sql.{Encoder, Encoders, Row, SaveMode, SparkSession}
+import org.apache.spark.sql.functions._
+
+import scala.collection.JavaConversions._
+
+case class EvoaMeasure(tagname: String, timestamp: Long, value: Double, quality: Double)
+
+
+object LoaderMode extends Enumeration {
+  type LoaderMode = Value
+
+  val PRELOAD = Value("preload")
+  val CHUNK = Value("chunk")
+}
+
+case class LoaderOptions(mode: LoaderMode, in: String, out: String, master: String, appName: String, brokers: scala.Option[String], lookup: scala.Option[String], chunkSize: Int, saxAlphabetSize:Int, saxStringLength: Int, useKerberos: Boolean)
+
+
+/**
+  * @author Thomas Bailet @Hurence
+  */
+object App {
+
+
+  val DEFAULT_CHUNK_SIZE = 2000
+  val DEFAULT_SAX_ALPHABET_SIZE = 7
+  val DEFAULT_SAX_STRING_LENGTH = 100
+
+  def parseCommandLine(args: Array[String]): LoaderOptions = {
+    // Commande lien management
+    val parser = new GnuParser
+    val options = new Options
+
+
+    val helpMsg = "Print this message."
+    val help = new Option("help", helpMsg)
+    options.addOption(help)
+
+    // Mode
+    OptionBuilder.withArgName("mode")
+    OptionBuilder.withLongOpt("loading-mode")
+    OptionBuilder.isRequired
+    OptionBuilder.hasArg
+    OptionBuilder.withDescription("preload or chunk : preload takes local csv files from input-path and partition data into year/month/code_install/name output-path whereas chunk takes hdfs input-path preloaded data and chunk it to kafka")
+    val mode = OptionBuilder.create("mode")
+    options.addOption(mode)
+
+    // Input Path
+    OptionBuilder.withArgName("in")
+    OptionBuilder.withLongOpt("input-path")
+    OptionBuilder.isRequired
+    OptionBuilder.hasArg
+    OptionBuilder.withDescription("input local folder for preload mode and input hdfs folder for chunk mode")
+    val in = OptionBuilder.create("in")
+    options.addOption(in)
+
+    // Output Path
+    OptionBuilder.withArgName("out")
+    OptionBuilder.withLongOpt("output-path")
+    OptionBuilder.isRequired
+    OptionBuilder.hasArg
+    OptionBuilder.withDescription("output local folder for preload mode and output kafka topic for chunk mode")
+    val out = OptionBuilder.create("out")
+    options.addOption(out)
+
+    // Kafka brokers
+    OptionBuilder.withArgName("brokers")
+    OptionBuilder.withLongOpt("kafka-brokers")
+    OptionBuilder.hasArg
+    OptionBuilder.withDescription("kafka brokers for chunk mode")
+    val brokers = OptionBuilder.create("brokers")
+    options.addOption(brokers)
+
+    // Spark master
+    OptionBuilder.withArgName("master")
+    OptionBuilder.withLongOpt("spark-master")
+    OptionBuilder.hasArg
+    OptionBuilder.isRequired
+    OptionBuilder.withDescription("spark master")
+    val master = OptionBuilder.create("master")
+    options.addOption(master)
+
+    // Lookup path
+    OptionBuilder.withArgName("lookup")
+    OptionBuilder.withLongOpt("lookup-path")
+    OptionBuilder.hasArg
+    OptionBuilder.withDescription("csv lookup ath for join")
+    val lookup = OptionBuilder.create("lookup")
+    options.addOption(lookup)
+
+    // Chunk size
+    OptionBuilder.withArgName("chunks")
+    OptionBuilder.withLongOpt("chunks-size")
+    OptionBuilder.hasArg
+    OptionBuilder.withDescription("num points in a chunk, default 2000")
+    val chunk = OptionBuilder.create("chunks")
+    options.addOption(chunk)
+
+    // SAX Alphabet size
+    OptionBuilder.withArgName("sa")
+    OptionBuilder.withLongOpt("sax-alphabet-size")
+    OptionBuilder.hasArg
+    OptionBuilder.withDescription("size of alphabet, default 7")
+    val sa = OptionBuilder.create("sa")
+    options.addOption(sa)
+
+    // SAX String length
+    OptionBuilder.withArgName("sl")
+    OptionBuilder.withLongOpt("sax-length")
+    OptionBuilder.hasArg
+    OptionBuilder.withDescription("num points in a chunk, default 100")
+    val sl = OptionBuilder.create("sl")
+    options.addOption(sl)
+
+    // SAX String length
+    OptionBuilder.withArgName("kb")
+    OptionBuilder.withLongOpt("kerberos")
+    OptionBuilder.withDescription("do we use kerberos ?")
+    val kb = OptionBuilder.create("kb")
+    options.addOption(kb)
+
+    // parse the command line arguments
+    val line = parser.parse(options, args)
+    val loadingMode = LoaderMode.withName(line.getOptionValue("mode").toLowerCase)
+    val inputPath = line.getOptionValue("in")
+    val outputPath = line.getOptionValue("out")
+    val sparkMaster = line.getOptionValue("master")
+    val useKerberos = if (line.hasOption("kb")) true else false
+    val lookupPath = if (line.hasOption("lookup")) Some(line.getOptionValue("lookup")) else None
+    val kafkaBrokers = if (line.hasOption("brokers")) Some(line.getOptionValue("brokers")) else None
+    val chunksSize = if (line.hasOption("chunks")) line.getOptionValue("chunks").toInt else DEFAULT_CHUNK_SIZE
+    val alphabetSize = if (line.hasOption("sa")) line.getOptionValue("sa").toInt else DEFAULT_SAX_ALPHABET_SIZE
+    val saxStringLength = if (line.hasOption("sl")) line.getOptionValue("sl").toInt else DEFAULT_SAX_STRING_LENGTH
+
+
+    if (loadingMode == LoaderMode.CHUNK && kafkaBrokers.isEmpty)
+      throw new IllegalArgumentException(s"kafka broker must not be empty with $loadingMode mode")
+
+    val appName = loadingMode match {
+      case LoaderMode.PRELOAD => "EvoaPreloader"
+      case LoaderMode.CHUNK => "EvoaChunker"
+      case _ => throw new IllegalArgumentException(s"unknown $loadingMode mode")
+    }
+
+    LoaderOptions(loadingMode, inputPath, outputPath, sparkMaster, appName, kafkaBrokers, lookupPath, chunksSize, alphabetSize, saxStringLength, useKerberos)
+
+
+  }
+
+
+  def preload(options: LoaderOptions, spark: SparkSession): Unit = {
+
+    import spark.implicits._
+
+    // Define formats
+    val csvRegexp = "((\\w+)\\.?(\\w+-?\\w+-?\\w+)?\\.?(\\w+)?)"
+    val dateFmt = "dd/MM/yyyy HH:mm:ss"
+
+    // Load raw data
+    val rawMeasuresDF = spark.read.format("csv")
+      .option("sep", ";")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .load(options.in)
+
+    // Then sort and split columns
+    val measuresDF = if (options.lookup.isDefined) {
+      val lookupDF = spark.read.format("csv")
+        .option("sep", ";")
+        .option("inferSchema", "true")
+        .option("header", "true")
+        .option("encoding", "ISO-8859-1")
+        .load(options.lookup.get)
+
+      rawMeasuresDF.join(lookupDF, "tagname")
+        .withColumn("time_ms", unix_timestamp($"timestamp", dateFmt) * 1000)
+        .withColumn("year", year(to_date($"timestamp", dateFmt)))
+        .withColumn("month", month(to_date($"timestamp", dateFmt)))
+        .withColumn("day", dayofmonth(to_date($"timestamp", dateFmt)))
+        .withColumn("name", regexp_extract($"tagname", csvRegexp, 1))
+        .withColumn("code_install", regexp_extract($"tagname", csvRegexp, 2))
+        .withColumn("sensor", regexp_extract($"tagname", csvRegexp, 3))
+        .withColumn("numeric_type", regexp_extract($"tagname", csvRegexp, 4))
+        .select("name", "value", "quality", "code_install", "sensor", "numeric_type", "description", "engunits", "timestamp", "time_ms", "year", "month", "day")
+        .sort(asc("name"), asc("time_ms"))
+        .dropDuplicates()
+    } else {
+
+      rawMeasuresDF
+        .withColumn("time_ms", unix_timestamp($"timestamp", dateFmt) * 1000)
+        .withColumn("year", year(to_date($"timestamp", dateFmt)))
+        .withColumn("month", month(to_date($"timestamp", dateFmt)))
+        .withColumn("day", dayofmonth(to_date($"timestamp", dateFmt)))
+        .withColumn("name", regexp_extract($"tagname", csvRegexp, 1))
+        .withColumn("code_install", regexp_extract($"tagname", csvRegexp, 2))
+        .withColumn("sensor", regexp_extract($"tagname", csvRegexp, 3))
+        .withColumn("numeric_type", regexp_extract($"tagname", csvRegexp, 4))
+        .select("name", "value", "quality", "code_install", "sensor", "timestamp", "time_ms", "year", "month", "day")
+        .sort(asc("name"), asc("time_ms"))
+        .dropDuplicates()
+    }
+
+    // save this to output path
+    measuresDF
+      .write
+      .mode(SaveMode.Append)
+      .partitionBy("year", "month", "code_install", "name")
+      .format("csv")
+      .option("header", "true")
+      .save(options.out)
+
+
+    println("Preloading done")
+  }
+
+  def chunk(options: LoaderOptions, spark: SparkSession) : Unit = {
+
+    import spark.implicits._
+
+    val testDF = spark.read.format("csv")
+      .option("sep", ",")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .load(options.in)
+      .cache()
+
+
+    val n = testDF.agg(countDistinct($"name")).collect().head.getLong(0).toInt
+
+
+
+    implicit val enc: Encoder[TimeSeriesRecord] = org.apache.spark.sql.Encoders.kryo[TimeSeriesRecord]
+
+    val tsDF = testDF.repartition(n, $"name")
+      .sortWithinPartitions($"name", $"time_ms")
+      .mapPartitions(partition => {
+
+        // Init the Timeserie processor
+        val tsProcessor = new TimeseriesConverter()
+        val context = new StandardProcessContext(tsProcessor, "")
+        context.setProperty(TimeseriesConverter.GROUPBY.getName, "name")
+        context.setProperty(TimeseriesConverter.METRIC.getName,
+          s"min;max;avg;trend;outlier;sax:${options.saxAlphabetSize},0.01,${options.saxStringLength}")
+        tsProcessor.init(context)
+
+        // Slide over each group
+        partition.toList
+          .groupBy(row => row.getString(row.fieldIndex("name")))
+          .flatMap(group => group._2
+            .sliding(options.chunkSize, options.chunkSize)
+            .map(subGroup => tsProcessor.toTimeseriesRecord(group._1, subGroup.toList))
+            .map(ts => (ts.getId, tsProcessor.serialize(ts)))
+          )
+          .iterator
+      }).toDF("key", "value")
+
+
+    // Write this chunk dataframe to Kafka
+    if(options.useKerberos){
+      tsDF
+        .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+        .write
+        .format("kafka")
+        .option("kafka.bootstrap.servers", options.brokers.get)
+        .option("kafka.security.protocol", "SASL_PLAINTEXT")
+        .option("kafka.sasl.kerberos.service.name", "kafka")
+        .option("topic", options.out)
+        .save()
+    }else{
+      tsDF
+        .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+        .write
+        .format("kafka")
+        .option("kafka.bootstrap.servers", options.brokers.get)
+        .option("topic", options.out)
+        .save()
+    }
+
+
+  }
+
+  /**
+    *
+    * @param args
+    */
+  def main(args: Array[String]): Unit = {
+
+    // get arguments
+    val options = parseCommandLine(args)
+
+    // setup spark session
+    val spark = SparkSession.builder
+      .appName(options.appName)
+      .master(options.master)
+      .getOrCreate()
+
+    // process
+    options.mode match {
+      case LoaderMode.PRELOAD => preload(options, spark)
+      case LoaderMode.CHUNK => chunk(options, spark)
+      case _ => println(s"unknown loader mode : $LoaderMode")
+    }
+  }
+
+}
---
+++ loader/src/main/scala/com/hurence/historian/TimeseriesConverter.java
@@ -0,0 +1,271 @@
+package com.hurence.historian;
+
+import com.hurence.logisland.component.InitializationException;
+import com.hurence.logisland.component.PropertyDescriptor;
+import com.hurence.logisland.processor.AbstractProcessor;
+import com.hurence.logisland.processor.ProcessContext;
+import com.hurence.logisland.processor.Processor;
+import com.hurence.logisland.record.*;
+import com.hurence.logisland.serializer.KryoSerializer;
+import com.hurence.logisland.timeseries.MetricTimeSeries;
+import com.hurence.logisland.timeseries.converter.compaction.BinaryCompactionConverter;
+import com.hurence.logisland.timeseries.functions.*;
+import com.hurence.logisland.timeseries.metric.MetricType;
+import com.hurence.logisland.timeseries.query.QueryEvaluator;
+import com.hurence.logisland.timeseries.query.TypeFunctions;
+import com.hurence.logisland.util.string.BinaryEncodingUtils;
+import com.hurence.logisland.validator.StandardValidators;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.spark.sql.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TimeseriesConverter extends AbstractProcessor {
+
+    public static final PropertyDescriptor GROUPBY = new PropertyDescriptor.Builder()
+            .name("groupby")
+            .description("The field the chunk should be grouped by")
+            .required(false)
+            .addValidator(StandardValidators.COMMA_SEPARATED_LIST_VALIDATOR)
+            .defaultValue("")
+            .build();
+
+
+    public static final PropertyDescriptor METRIC = new PropertyDescriptor.Builder()
+            .name("metric")
+            .description("The chronix metric to calculate for the chunk")
+            .required(false)
+            .addValidator(StandardValidators.SEMICOLON_SEPARATED_LIST_VALIDATOR)
+            .build();
+
+    public List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        final List<PropertyDescriptor> descriptors = new ArrayList<>();
+        descriptors.add(GROUPBY);
+        descriptors.add(METRIC);
+        return descriptors;
+    }
+
+    private Logger logger = LoggerFactory.getLogger(TimeseriesConverter.class);
+
+    private List<ChronixTransformation> transformations = Collections.emptyList();
+    private List<ChronixAggregation> aggregations = Collections.emptyList();
+    private List<ChronixAnalysis> analyses = Collections.emptyList();
+    private List<ChronixEncoding> encodings = Collections.emptyList();
+    private FunctionValueMap functionValueMap = new FunctionValueMap(0, 0, 0, 0);
+
+    private BinaryCompactionConverter converter;
+    private List<String> groupBy;
+    private final KryoSerializer serializer = new KryoSerializer(true);
+
+
+    @Override
+    public void init(ProcessContext context) throws InitializationException {
+        super.init(context);
+
+        // init binary converter
+        final String[] groupByArray = context.getPropertyValue(GROUPBY).asString().split(",");
+        groupBy = Arrays.stream(groupByArray)
+                .filter(StringUtils::isNotBlank)
+                .collect(Collectors.toList());
+        BinaryCompactionConverter.Builder builder = new BinaryCompactionConverter.Builder();
+        converter = builder.build();
+
+        // init metric functions
+        if (context.getPropertyValue(METRIC).isSet()) {
+            String[] metric = {"metric{" + context.getPropertyValue(METRIC).asString() + "}"};
+
+            TypeFunctions functions = QueryEvaluator.extractFunctions(metric);
+
+            analyses = functions.getTypeFunctions(new MetricType()).getAnalyses();
+            aggregations = functions.getTypeFunctions(new MetricType()).getAggregations();
+            transformations = functions.getTypeFunctions(new MetricType()).getTransformations();
+            encodings = functions.getTypeFunctions(new MetricType()).getEncodings();
+            functionValueMap = new FunctionValueMap(aggregations.size(), analyses.size(), transformations.size(), encodings.size());
+        }
+    }
+
+    @Override
+    public Collection<Record> process(ProcessContext processContext, Collection<Record> collection) {
+        return null;
+    }
+
+
+    /**
+     * gets the kryo bytes representation of a ts record
+     *
+     * @param tsRecord
+     * @return
+     */
+    public byte[] serialize(TimeSeriesRecord tsRecord) {
+
+        // has Id
+        final String hashString = DigestUtils.sha256Hex(tsRecord.getField(FieldDictionary.CHUNK_VALUE).asBytes());
+        tsRecord.setId(hashString);
+
+
+        // add technical fields
+
+
+        // encode chunk_value to base64
+        Field f = tsRecord.getField(FieldDictionary.CHUNK_VALUE);
+        if (f != null) {
+            if (!(f.getType() == FieldType.BYTES || f.getType() == FieldType.NULL)) {
+                tsRecord.addError("FIELD TYPE", getLogger(),
+                        "Field type '{}' is not an array of bytes",
+                        new Object[]{f.getName()});
+            } else {
+                byte[] content = f.asBytes();
+                if (content != null) {
+                    try {
+                        tsRecord.setStringField(FieldDictionary.CHUNK_VALUE, BinaryEncodingUtils.encode(content));
+                    } catch (Exception e) {
+                        tsRecord.addError("PROCESSING ERROR", getLogger(),
+                                "Unable to encode field '{}' : {}",
+                                new Object[]{f.getName(), e.getMessage()});
+                    }
+                }
+            }
+        }
+
+        // get thoses bytes
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        serializer.serialize(baos, tsRecord);
+        try {
+            baos.close();
+            return baos.toByteArray();
+        } catch (IOException e) {
+            tsRecord.addError("PROCESSING ERROR", getLogger(),
+                    "Unable to serialize field record : {}",
+                    new Object[]{e.getMessage()});
+            return null;
+        }
+    }
+
+
+    /**
+     * Converts a list of rows to a timeseries chunk
+     *
+     * @param metricName
+     * @param rows
+     * @return
+     */
+    public TimeSeriesRecord toTimeseriesRecord(String metricName, List<Row> rows) {
+
+        // Convert first to logisland records
+        List<Record> groupedRecords = new ArrayList<>();
+        for (Row r : rows) {
+            long time = r.getLong(r.fieldIndex("time_ms"));
+
+            double value=Double.MAX_VALUE;
+            try {
+                value = r.getDouble(r.fieldIndex("value"));
+            } catch (Exception e) {
+                try {
+                    value = Double.parseDouble(r.getString(r.fieldIndex("value")));
+                } catch (Exception e2) {
+                    logger.error("unable to parse value for row : " + r.toString());
+                }
+
+            }
+
+            double quality=Double.MAX_VALUE;
+            try {
+                quality = r.getDouble(r.fieldIndex("quality"));
+            } catch (Exception e) {
+                try {
+                    quality = Double.parseDouble(r.getString(r.fieldIndex("quality")));
+                } catch (Exception e2) {
+                    logger.error("unable to parse quality for row : " + r.toString());
+                }
+            }
+
+            String name = r.getString(r.fieldIndex("name"));
+            String codeInstall = "";
+            try {
+                codeInstall = r.getString(r.fieldIndex("code_install"));
+            } catch (Exception ex) {
+                //do nothing
+            }
+            String sensor = "";
+            try {
+                sensor = r.getString(r.fieldIndex("sensor"));
+            } catch (Exception ex) {
+                //do nothing
+            }
+
+            String year = "";
+            try {
+                year = String.valueOf(r.getInt(r.fieldIndex("year")));
+            } catch (Exception ex) {
+                //do nothing
+            }
+            String month = "";
+            try {
+                month = String.valueOf(r.getInt(r.fieldIndex("month")));
+            } catch (Exception ex) {
+                //do nothing
+            }
+            String day = "";
+            try {
+                day = String.valueOf(r.getInt(r.fieldIndex("day")));
+            } catch (Exception ex) {
+                //do nothing
+            }
+
+            Record record = new StandardRecord(RecordDictionary.TIMESERIES)
+                    .setStringField(FieldDictionary.RECORD_NAME, metricName)
+                    .setDoubleField(FieldDictionary.RECORD_VALUE, value)
+                    .setDoubleField("quality", quality)
+                    .setStringField("name", name)
+                    .setStringField("year", year)
+                    .setStringField("month", month)
+                    .setStringField("day", day)
+                    .setStringField("code_install", codeInstall)
+                    .setStringField("sensor", sensor)
+                    .setTime(time);
+
+            groupedRecords.add(record);
+        }
+
+
+        TimeSeriesRecord tsRecord = converter.chunk(groupedRecords);
+        MetricTimeSeries timeSeries = tsRecord.getTimeSeries();
+
+        functionValueMap.resetValues();
+
+        transformations.forEach(transfo -> transfo.execute(timeSeries, functionValueMap));
+        analyses.forEach(analyse -> analyse.execute(timeSeries, functionValueMap));
+        aggregations.forEach(aggregation -> aggregation.execute(timeSeries, functionValueMap));
+        encodings.forEach(encoding -> encoding.execute(timeSeries, functionValueMap));
+
+        for (int i = 0; i < functionValueMap.sizeOfAggregations(); i++) {
+            String name = functionValueMap.getAggregation(i).getQueryName();
+            double value = functionValueMap.getAggregationValue(i);
+            tsRecord.setField("chunk_" + name, FieldType.DOUBLE, value);
+        }
+
+        for (int i = 0; i < functionValueMap.sizeOfAnalyses(); i++) {
+            String name = functionValueMap.getAnalysis(i).getQueryName();
+            boolean value = functionValueMap.getAnalysisValue(i);
+            tsRecord.setField("chunk_" + name, FieldType.BOOLEAN, value);
+        }
+
+        for (int i = 0; i < functionValueMap.sizeOfEncodings(); i++) {
+            String name = functionValueMap.getEncoding(i).getQueryName();
+            String value = functionValueMap.getEncodingValue(i);
+            tsRecord.setField("chunk_" + name, FieldType.STRING, value);
+        }
+
+
+        return tsRecord;
+    }
+
+
+}
