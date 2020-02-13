521a5cc004c5bf030dfe42f1d8574b0e45c13d29
majdeddine nemsi <majdeddine.nemsi.94@gmail.com>
2020-02-13 16:03:06 +0100

    updated and commented work

--- timeseries/src/test/groovy/com/hurence/logisland/timeseries/timeseries/SaxAnalyserTest.groovy
+++ timeseries/src/test/groovy/com/hurence/logisland/timeseries/timeseries/SaxAnalyserTest.groovy
@@ -5,6 +5,10 @@ import spock.lang.Unroll
 import static com.hurence.logisland.timeseries.sax.SaxAnalyzer.anomalyDetect
 import static com.hurence.logisland.timeseries.sax.SaxAnalyzer.saxThreshold
 
+/**
+ * Unit test for the sax coding anomalies
+ * @author Mejdeddine Nemsi
+ */
 class SaxAnalyserTest extends Specification{
     @Unroll
     def "test anomaly detection in sax strings data"(){
@@ -13,18 +17,19 @@ class SaxAnalyserTest extends Specification{
         def listin = []
 
         def listout =[]
-        def s1 = "dededdecccdcedeedccddcddccedeeeeeedceefedbdbccdcccp"//usual sensor output
-        def s2 ="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"//  one type character
-        def s3 = "b"//one character output
-        def s4 = "saxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxh"//pattern anomaly (pattern : sax)
-        def s5 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaa"//one anomaly in one type character output
-        def s6 = "eeffddddddcdffffeeededdddcedcdfdeddcccdcdedcbbbabadcddcddcefeededcddccedddceddKcedcdcbcceeddddcceeded"//unusual output length
+        def s1 = "dededdecccdcedeedccddcddccedeeeeeedceefedbdbccdcccp"//usual sensor output [49]
+        def s2 ="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"//  one type character [-1]
+        def s3 = "b"//one character output [-1]
+        def s4 = "saxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxsaxh"//pattern anomaly (pattern : sax) [50]
+        def s5 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaa"//one anomaly in one type character output [32,33]
+        def s6 = "eeffddddddcdffffeeededdddcedcdfdeddcccdcdedcbbbabadcddcddcefeededcddccedddceddKcedcdcbcceeddddcceeded"//unusual output length [49, 77, 78]
+        def s7 = ""//empty sensor output [-1]
 
-        listin << s1 << s2 << s3 << s4 <<s5 << s6
+        listin << s1 << s2 << s3 << s4 <<s5 << s6 <<s7
         for (i in listin){
             listout<<anomalyDetect(i, saxThreshold(i))
         }
         expect:
-        listout == [[49],[-1],[-1],[50],[32,33],[49, 77, 78]]
+        listout == [[49],[-1],[-1],[50],[32,33],[49, 77, 78],[-1]]
     }
 }
