0fccc46df6690d8f895e5fb3e87ce87f1b78dc5a
majdeddine nemsi <majdeddine.nemsi.94@gmail.com>
2020-02-13 16:03:06 +0100

    updated and commented work

--- timeseries/src/main/java/com/hurence/logisland/timeseries/sax/SaxAnalyzer.java
+++ timeseries/src/main/java/com/hurence/logisland/timeseries/sax/SaxAnalyzer.java
@@ -1,47 +1,89 @@
 package com.hurence.logisland.timeseries.sax;
 
-import com.sun.javafx.collections.MappingChange;
+        import java.util.*;
+        import java.util.stream.Collectors;
 
-import java.util.*;
-import java.util.stream.Collectors;
+        import static java.util.stream.Collectors.*;
+/**
+ * Class: anomaly detection in sax coding strings
+ * @author Mejdeddine Nemsi
+ * @version 1.0
+ *Example: input string "dededdecccdcedeedccddcddccedeeeeeedceefedbdbccdcccp"
+ *  calculate the array of distances between characters [1, 1, 1, 1, 0, 1, 2, 0, 0, 1, 1, 2, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 8, 6, 1, 1, 0, 0, 0, 0, 0, 1, 1, 2, 0, 1, 1, 1, 2, 2, 2, 1, 0, 1, 1, 0, 0, 13]
+ * count occurrences (0 -> 17, 1 -> 25, 6 -> 1, 13 -> 1, 2 -> 6, 8 -> 1)
+ * normalise occurrences number (0 -> 0.3333333333333333, 1 -> 0.49019607843137253, 6 -> 0.0196078431372549, 13 -> 0.0196078431372549, 2 -> 0.11764705882352941, 8 -> 0.0196078431372549)
+ * filter to less then 10% of total occurrences (6 -> 0.0196078431372549, 13 -> 0.0196078431372549)
+ * saxThreshold function returns list of last map keys : [6,13]
+ * we use these distances in anomalyDetect function which will return the positions of anomalies [25,26,50]
 
-import static java.util.stream.Collectors.*;
-
-
+ */
 public class SaxAnalyzer {
-
+    /**
+     *Method that counts the number of occurrence of intergers in an array
+     * @param numbersToProcess array of integer
+     * @return map of each element of input array and it's occurrences
+     */
     public static Map<Integer, Integer> countOccurrence(int[] numbersToProcess) {
-        int[] possibleNumbers = new int[30];
+        final int alphabeticMax = 30;
+        int[] possibleNumbers = new int[alphabeticMax];
         Map<Integer, Integer> result = new HashMap<Integer, Integer>();
 
         for (int i = 0; i < numbersToProcess.length; ++i) {
             possibleNumbers[numbersToProcess[i]] = possibleNumbers[numbersToProcess[i]] + 1;
             result.put(numbersToProcess[i], possibleNumbers[numbersToProcess[i]]);
         }
-
         return result;
     }
-    public static List<Integer> saxThreshold(String str){
-        double l = str.length();
-        //Transform our string into an array of characters
-        char[] strArray = new char[str.length()];
-        for (int i = 0; i < str.length(); i++)
-            strArray[i] = str.charAt(i);
-        //Array of distances between characters
-        int[] buff = new int[str.length()-1];
-        for (int j =0; j< str.length()-1; j++)
-            buff[j] = Math.abs((int)(strArray [j+1] - strArray[j]));
 
+    /**
+     * Method that converts a string into an array of characters
+     * @param str input sax coding string
+     * @return array of characters
+     */
+    public static char[] convArrayChar (String str){
+        char[] result = new char[str.length()];
+        for (int i = 0; i < str.length(); i++)
+            result[i] = str.charAt(i);
+        return result;
+    }
+
+    /**
+     * Method that counts the distance between characters in an array of characters
+     * @param strArray array of characters
+     * @return array of integers, empty list in case of empty input
+     */
+    public static int[] charDisAbs (char[] strArray){
+        if (strArray.length == 0){
+            int reslut[] = {};
+            return reslut;}
+        else{
+            int[] result1 = new int[strArray.length-1];
+            for (int i = 0; i < strArray.length-1; i++)
+                result1[i] = Math.abs(strArray [i+1] - strArray[i]);
+            return result1;}
+    }
+
+    /**
+     * Method that returns the searchs for abnormal distances between characters.
+     * Exemple:
+     *
+     * @param str string (sax coding string)
+     * @return list of abnormal distances between characters
+     */
+    public static List<Integer> saxThreshold(String str){
+        final double threshold = 0.1;
+        //Transform our string into an array of characters then into an array of distances between characters
+        int[] buff = charDisAbs(convArrayChar(str));
         //Counting occurrences
-        Map<Integer, Integer> buffCount = countOccurrence( buff);
+        Map<Integer, Integer> buffCount = countOccurrence(buff);
         //Normalize the occurrences
         Map<Integer, Double> buffCountNorm = buffCount.entrySet().stream().collect(toMap(e -> e.getKey(),
-                e -> e.getValue()/(l-1)));
+                e -> e.getValue()/((double)(str.length()-1))));
         //filter to less the 10% of total occurrences
         Map<Integer, Double> buffCountNormPer  = (buffCountNorm.entrySet().stream()
-                .filter(map -> map.getValue() <0.1)
+                .filter(map -> map.getValue() <threshold)
                 .collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue())));
-        //get the threshold : min of buffCountNormP keys
+        //convert the last map created into a list to be outputed
         List<Integer> finalResult = new ArrayList(buffCountNormPer.keySet());
         if (buffCountNormPer.isEmpty() == true)
             return Collections.emptyList();
@@ -49,27 +91,32 @@ public class SaxAnalyzer {
             return finalResult;
     }
 
+    /**
+     * Method that prints the anomalies positions in our sax coding string
+     * @param str string (sax coding string)
+     * @param thresh list of anomaly distances between characters in input string
+     * @return list of postions of anomalies in sax coding string
+     */
     public static List<Integer> anomalyDetect (String str, List<Integer> thresh){
-        //Method that prints the anomalies position in our string of data
+
         Boolean res = false;
         List<Integer> finalResult = new ArrayList<>();
         //Transform our string into an array of characters
-        char[] strArray = new char[str.length()];
-        for (int i = 0; i < str.length(); i++)
-            strArray[i] = str.charAt(i);
+        char[] strArray = convArrayChar(str);
         //Search for Anomalies
         for (int j = 0; j< str.length()-1; j++)
-            if( thresh.contains(Math.abs((int)(strArray [j+1] - strArray[j])))) {
+            if(thresh.contains(Math.abs(strArray [j+1] - strArray[j]))) {
                 res = true;
                 finalResult.add(j);
             }
         //In the case of no anomaly is detected
-        if (res == false) {finalResult.add(-1);}
-
+        if (res == false)
+            finalResult.add(-1);
 
         return finalResult;
-
-
     }
-
+//    public static void main (String[] args) {
+//    String str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaa";
+//        System.out.println(saxThreshold(str));
+//    }
 }
