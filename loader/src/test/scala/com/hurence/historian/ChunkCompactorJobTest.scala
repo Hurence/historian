9a46f6fa1f840ffe8e46cd77330a48cca669da55
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2020-03-06 17:38:39 +0100

    Implemented a first test to load data from solr. Refactored conf of job so we can test compactor. For now running the test failes with an exception.

--- loader/src/test/scala/com/hurence/historian/ChunkCompactorJobTest.scala
+++ loader/src/test/scala/com/hurence/historian/ChunkCompactorJobTest.scala
@@ -1,36 +1,35 @@
 package com.hurence.historian
 
-import com.hurence.historian.ChunkCompactorJob.ChunkCompactorOptions
-import com.hurence.logisland.record.TimeSeriesRecord
+import java.util
+import java.util.Arrays
+
+import com.hurence.historian.ChunkCompactorJob.{ChunkCompactorConf, ChunkCompactorJobOptions}
+import com.hurence.historian.solr.injector.{SolrInjector, SolrInjectorMultipleMetricSpecificPoints}
+import com.hurence.historian.solr.util.SolrITHelper
+import com.hurence.logisland.record.{Point, TimeSeriesRecord}
 import com.hurence.unit5.extensions.{SolrExtension, SparkExtension}
 import org.apache.solr.client.solrj.SolrClient
 import org.apache.spark.sql.{Dataset, SparkSession}
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.{BeforeAll, Test}
 import org.junit.jupiter.api.extension.ExtendWith
 import org.slf4j.{Logger, LoggerFactory}
 import org.junit.jupiter.api.Assertions._
+import org.testcontainers.containers.DockerComposeContainer
 
 @ExtendWith(Array(classOf[SolrExtension], classOf[SparkExtension]))
-class ChunkCompactorJobTest {
-    private val LOGGER = LoggerFactory.getLogger(classOf[ChunkCompactorJobTest])
+class ChunkCompactorJobTest(container : (DockerComposeContainer[SELF]) forSome {type SELF <: DockerComposeContainer[SELF]}) {
 
-    val testOptions = ChunkCompactorOptions("local[2]", "zookeeper:2181", "historian", "loader-test", 10, 2, 2, false, 2019, 6, 19)
-    val compactor = new ChunkCompactorJob(testOptions)
+    //    val testOptions = ChunkCompactorJobOptions("local[]", "loader-test", false, SolrExtension.getZkUrl(container),  SolrITHelper.COLLECTION_HISTORIAN, 1440, 7, 100, 2019, 6, 19)
+    val compactorConf = ChunkCompactorConf(SolrExtension.getZkUrl(container),  SolrITHelper.COLLECTION_HISTORIAN, 1440, 7, 100, 2019, 6, 19)
+    val compactor = new ChunkCompactorJob(compactorConf)
 
     private var loadedFromSolr: Dataset[TimeSeriesRecord] = null
     private var chunked: Dataset[TimeSeriesRecord] = null
 
-
-    @Test
-    def testOK(client: SolrClient) = {
-        LOGGER.info("client : {}", client)
-        assertTrue(true)
-    }
-
     @Test
     def testLoading(sparkSession: SparkSession) = {
-//        loadedFromSolr = compactor.loadDataFromSolR(sparkSession, s"name:*")
-        //TODO test it
+        loadedFromSolr = compactor.loadDataFromSolR(sparkSession, s"name:*")
+        loadedFromSolr.show(100)
         assertTrue(true)
     }
 
@@ -49,4 +48,38 @@ class ChunkCompactorJobTest {
     }
 }
 
+object ChunkCompactorJobTest {
+    private val LOGGER = LoggerFactory.getLogger(classOf[ChunkCompactorJobTest])
+
+    @BeforeAll
+    def initHistorianAndDeployVerticle(client: SolrClient): Unit = {
+        SolrITHelper.initHistorianSolr(client)
+        LOGGER.info("Indexing some documents in {} collection", SolrITHelper.COLLECTION_HISTORIAN)
+        val injector: SolrInjector =
+            new SolrInjectorMultipleMetricSpecificPoints(
+                util.Arrays.asList("temp_a", "temp_b", "maxDataPoints"),
+                util.Arrays.asList(
+                    util.Arrays.asList(
+                        new Point(0, 1477895624866L, 622),
+                        new Point(0, 1477916224866L, -3),
+                        new Point(0, 1477917224866L, 365)),
+                    util.Arrays.asList(new Point(0, 1477895624866L, 861),
+                        new Point(0, 1477917224866L, 767)),
+                    util.Arrays.asList( //maxDataPoints we are not testing value only sampling
+                        new Point(0, 1477895624866L, 1),
+                        new Point(0, 1477895624867L, 1),
+                        new Point(0, 1477895624868L, 1),
+                        new Point(0, 1477895624869L, 1),
+                        new Point(0, 1477895624870L, 1),
+                        new Point(0, 1477895624871L, 1),
+                        new Point(0, 1477895624872L, 1),
+                        new Point(0, 1477895624873L, 1),
+                        new Point(0, 1477895624874L, 1),
+                        new Point(0, 1477895624875L, 1),
+                        new Point(0, 1477895624876L, 1))
+                ))
+        injector.injectChunks(client)
+        LOGGER.info("Indexed some documents in {} collection", SolrITHelper.COLLECTION_HISTORIAN)
+    }
+}
 
