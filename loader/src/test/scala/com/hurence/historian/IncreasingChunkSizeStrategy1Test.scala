c31afb9467f6e1e17b912a8b629310e993ad1113
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2020-03-13 11:07:22 +0100

    Fixed tests.

--- loader/src/test/scala/com/hurence/historian/IncreasingChunkSizeStrategy1Test.scala
+++ loader/src/test/scala/com/hurence/historian/IncreasingChunkSizeStrategy1Test.scala
@@ -6,7 +6,15 @@ import org.testcontainers.containers.DockerComposeContainer
 class IncreasingChunkSizeStrategy1Test(container: (DockerComposeContainer[SELF]) forSome {type SELF <: DockerComposeContainer[SELF]})
   extends AbstractIncreasingChunkSizeTest(container) {
 
-  override def getCompactorFactory() = {
-    (conf: ChunkCompactorConf) => new ChunkCompactorJobStrategy1(conf)
+  val compactorConf: ChunkCompactorConf = ChunkCompactorConf(zkUrl, historianCollection,
+    chunkSize = 10,
+    saxAlphabetSize = 2,
+    saxStringLength = 3,
+    year = year,
+    month = month,
+    day = day)
+
+  override def createCompactor() = {
+    new ChunkCompactorJobStrategy1(compactorConf)
   }
 }
