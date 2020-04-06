92b4e3def3a6634c004779f45f7e1ccb6300f56b
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2020-04-06 16:15:27 +0200

    Added log and modified signatur method of conversion.

--- gateway/src/main/java/com/hurence/webapiservice/historian/compatibility/JsonSchemaCompatibilityUtil.java
+++ gateway/src/main/java/com/hurence/webapiservice/historian/compatibility/JsonSchemaCompatibilityUtil.java
@@ -1,13 +1,18 @@
 package com.hurence.webapiservice.historian.compatibility;
 
 import com.hurence.historian.modele.HistorianFields;
+import com.hurence.webapiservice.historian.impl.SolrHistorianServiceImpl;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class JsonSchemaCompatibilityUtil {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(JsonSchemaCompatibilityUtil.class);
 
     public static final List<String> multivaluedListFieldsToConvertToMonoField = Arrays.asList(
             HistorianFields.CHUNK_DAY,
@@ -19,7 +24,8 @@ public class JsonSchemaCompatibilityUtil {
             HistorianFields.RESPONSE_CHUNK_TREND_FIELD
     );
 
-    public static JsonObject convertSchema0ToCurrent(JsonObject chunk) {
+    public static void convertJsonSchema0ToCurrent(JsonObject chunk) {
+        LOGGER.debug("chunk version {} is {}", SchemaVersion.VERSION_0, chunk);
         multivaluedListFieldsToConvertToMonoField.forEach(f -> {
             if (chunk.containsKey(f)) {
                 Object value = chunk.getValue(f);
@@ -31,6 +37,6 @@ public class JsonSchemaCompatibilityUtil {
                 }
             }
         });
-        return chunk;
+        LOGGER.debug("chunk version {} (converted) is {}", SchemaVersion.CURRENT_VERSION, chunk);
     }
 }
