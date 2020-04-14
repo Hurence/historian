cdc6f84e090d1adbd1e60b4886aaf4c57e2bba54
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2020-04-14 10:51:03 +0200

    Fixed a NPE in configuration loading of historian Verticle.

--- historian-server/src/main/java/com/hurence/webapiservice/historian/HistorianVerticle.java
+++ historian-server/src/main/java/com/hurence/webapiservice/historian/HistorianVerticle.java
@@ -70,19 +70,11 @@ public class HistorianVerticle extends AbstractVerticle {
   @Override
   public void start(Promise<Void> promise) throws Exception {
     LOGGER.debug("deploying {} verticle with config : {}", HistorianVerticle.class.getSimpleName(), config().encodePrettily());
-    //general conf
-    final String address = config().getString(CONFIG_HISTORIAN_ADDRESS, "historian");
-    final long limitNumberOfPoint = config().getLong(CONFIG_LIMIT_NUMBER_OF_POINT, 50000L);
-    final long limitNumberOfChunks = config().getLong(CONFIG_LIMIT_NUMBER_OF_CHUNK, 50000L);
-    final JsonObject grafana = config().getJsonObject(CONFIG_API_HISTORAIN).getJsonObject(CONFIG_GRAFANA_HISTORAIN);
-    //solr conf
+
     final JsonObject slrConfig = config().getJsonObject(CONFIG_ROOT_SOLR);
     final int connectionTimeout = slrConfig.getInteger(CONFIG_SOLR_CONNECTION_TIMEOUT, 10000);
     final int socketTimeout = slrConfig.getInteger(CONFIG_SOLR_SOCKET_TIMEOUT, 60000);
     final boolean useZookeeper = slrConfig.getBoolean(CONFIG_SOLR_USE_ZOOKEEPER, false);
-    final String chunkCollection = slrConfig.getString(CONFIG_SOLR_CHUNK_COLLECTION, "historian");
-    final String annotationCollection = slrConfig.getString(CONFIG_SOLR_ANNOTATION_COLLECTION, "annotation");
-
 
     if (!slrConfig.containsKey(CONFIG_SOLR_STREAM_ENDPOINT))
       throw new IllegalArgumentException(String.format("key %s is needed in solr config of historian verticle conf.",
@@ -116,6 +108,12 @@ public class HistorianVerticle extends AbstractVerticle {
       this.client = new HttpSolrClient.Builder(getStringListIfExist(slrConfig, CONFIG_SOLR_URLS).get().get(0) ).build();
     }
 
+    final String chunkCollection = slrConfig.getString(CONFIG_SOLR_CHUNK_COLLECTION, "historian");
+    final String annotationCollection = slrConfig.getString(CONFIG_SOLR_ANNOTATION_COLLECTION, "annotation");
+    final String address = config().getString(CONFIG_HISTORIAN_ADDRESS, "historian");
+    final long limitNumberOfPoint = config().getLong(CONFIG_LIMIT_NUMBER_OF_POINT, 50000L);
+    final long limitNumberOfChunks = config().getLong(CONFIG_LIMIT_NUMBER_OF_CHUNK, 50000L);
+    final int maxNumberOfTargetReturned = getMaxNumberOfTargetReturned();
     SolrHistorianConf historianConf = new SolrHistorianConf();
     historianConf.client = client;
     historianConf.chunkCollection = chunkCollection;
@@ -125,7 +123,7 @@ public class HistorianVerticle extends AbstractVerticle {
     historianConf.limitNumberOfChunks = limitNumberOfChunks;
     historianConf.sleepDurationBetweenTry = slrConfig.getLong(CONFIG_SOLR_SLEEP_BETWEEEN_TRY, 10000L);;
     historianConf.numberOfRetryToConnect = slrConfig.getInteger(CONFIG_SOLR_NUMBER_CONNECTION_ATTEMPT, 3);;
-    historianConf.maxNumberOfTargetReturned = grafana.getJsonObject(CONFIG_SEARCH_HISTORAIN).getInteger(CONFIG_DEFAULT_SIZE_HISTORAIN, 100);
+    historianConf.maxNumberOfTargetReturned = maxNumberOfTargetReturned;
     String schemaVersion = config().getString(CONFIG_SCHEMA_VERSION, SchemaVersion.CURRENT_VERSION.toString());
     historianConf.schemaVersion = SchemaVersion.valueOf(schemaVersion);
 
@@ -140,6 +138,19 @@ public class HistorianVerticle extends AbstractVerticle {
         promise.fail(ready.cause());
       }
     });
+  }
+
+  private int getMaxNumberOfTargetReturned() {
+    if (config().containsKey(CONFIG_API_HISTORAIN) &&
+            config().getJsonObject(CONFIG_API_HISTORAIN).containsKey(CONFIG_GRAFANA_HISTORAIN) &&
+            config().getJsonObject(CONFIG_API_HISTORAIN).getJsonObject(CONFIG_GRAFANA_HISTORAIN).containsKey(CONFIG_SEARCH_HISTORAIN)
+    ) {
+      return config().getJsonObject(CONFIG_API_HISTORAIN)
+              .getJsonObject(CONFIG_GRAFANA_HISTORAIN)
+              .getJsonObject(CONFIG_SEARCH_HISTORAIN)
+              .getInteger(CONFIG_DEFAULT_SIZE_HISTORAIN, 100);
+    }
+    return 100;
   }
 
   private Optional<List<String>> getStringListIfExist(JsonObject config, String key) {
