1a5a5bba143e148d557cec3d698d61eaf183bb70
FeizNouri <feiznouri@gmail.com>
2020-04-17 14:55:03 +0100

    deleting unuseful test files

--- historian-modele/src/main/java/com/hurence/historian/modele/HistorianFields.java
+++ historian-modele/src/main/java/com/hurence/historian/modele/HistorianFields.java
@@ -38,7 +38,7 @@ public class HistorianFields {
     public static String TIMESERIES_RESPONSE_FIELD = "timeseries";
     public static String TARGET_RESPONSE_FIELD = "target";
     public static String DATAPOINTS_RESPONSE_FIELD = "datapoints";
-    public static String ERRORS_RESPONSE_FIELD = "errors";
+    public static String ERRORS_RESPONSE_FIELD = "error";
 
     public static String RESPONSE_CHUNKS = "chunks";
     public static String RESPONSE_METRICS = "metrics";
