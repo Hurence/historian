020ff66507bf2155a5c6ddd7da16144aef4600d2
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2020-05-19 15:35:38 +0200

    Extracted all Trait in /TestFramework in its own file.

---
+++ loader/src/test/scala/com/hurence/test/framework/EventsimBuilder.scala
@@ -0,0 +1,30 @@
+package com.hurence.test.framework
+
+import java.util.UUID
+
+import com.hurence.historian.spark.solr.SolrCloudUtil
+import com.hurence.historian.spark.solr.util.EventsimUtil
+
+// Builder to be used by all the tests that need Eventsim data. All test methods will re-use the same collection name
+trait EventsimBuilder extends TestSuiteBuilder {
+
+  val collectionName: String = "EventsimTest-" + UUID.randomUUID().toString
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    SolrCloudUtil.buildCollection(zkHost, collectionName, null, numShards, cloudClient, sc)
+    EventsimUtil.defineTextFields(cloudClient, collectionName)
+    EventsimUtil.loadEventSimDataSet(zkHost, collectionName, sparkSession)
+  }
+
+  override def afterAll(): Unit = {
+    SolrCloudUtil.deleteCollection(collectionName, cluster)
+    super.afterAll()
+  }
+
+  def eventSimCount: Int = 1000
+
+  def fieldsCount: Int = 19
+
+  def numShards: Int = 2
+}
---
+++ loader/src/test/scala/com/hurence/test/framework/SolrCloudTestBuilder.scala
@@ -0,0 +1,65 @@
+package com.hurence.test.framework
+
+import java.io.File
+
+import com.hurence.historian.TestSolrCloudClusterSupport
+import com.lucidworks.spark.LazyLogging
+import org.apache.commons.io.FileUtils
+import org.apache.solr.client.solrj.impl.CloudSolrClient
+import org.apache.solr.cloud.MiniSolrCloudCluster
+import org.eclipse.jetty.servlet.ServletHolder
+import org.junit.Assert.assertTrue
+import org.restlet.ext.servlet.ServerServlet
+import org.scalatest.{BeforeAndAfterAll, Suite}
+
+trait SolrCloudTestBuilder extends BeforeAndAfterAll with LazyLogging { this: Suite =>
+
+  @transient var cluster: MiniSolrCloudCluster = _
+  @transient var cloudClient: CloudSolrClient = _
+  var zkHost: String = _
+  var testWorkingDir: File = _
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+
+    System.setProperty("jetty.testMode", "true")
+   // val solrXml = new File("src/test/resources/solr.xml")
+    val solrXml = new File(this.getClass.getClassLoader.getResource("solr.xml").getPath)
+    val solrXmlContents: String = TestSolrCloudClusterSupport.readSolrXml(solrXml)
+
+    val targetDir = new File("target")
+    if (!targetDir.isDirectory)
+      fail("Project 'target' directory not found at :" + targetDir.getAbsolutePath)
+
+    testWorkingDir = new File(targetDir, "scala-solrcloud-" + System.currentTimeMillis)
+    if (!testWorkingDir.isDirectory)
+      testWorkingDir.mkdirs
+
+    // need the schema stuff
+    val extraServlets: java.util.SortedMap[ServletHolder, String] = new java.util.TreeMap[ServletHolder, String]()
+
+    val solrSchemaRestApi : ServletHolder = new ServletHolder("SolrSchemaRestApi", classOf[ServerServlet])
+    solrSchemaRestApi.setInitParameter("org.restlet.application", "org.apache.solr.rest.SolrSchemaRestApi")
+    extraServlets.put(solrSchemaRestApi, "/schema/*")
+
+    cluster = new MiniSolrCloudCluster(1, null /* hostContext */,
+      testWorkingDir.toPath, solrXmlContents, extraServlets, null /* extra filters */)
+    cloudClient = cluster.getSolrClient
+    cloudClient.connect()
+
+    assertTrue(!cloudClient.getZkStateReader.getClusterState.getLiveNodes.isEmpty)
+    zkHost = cluster.getZkServer.getZkAddress
+  }
+
+  override def afterAll(): Unit = {
+    cloudClient.close()
+    cluster.shutdown()
+
+    if (testWorkingDir != null && testWorkingDir.isDirectory) {
+      FileUtils.deleteDirectory(testWorkingDir)
+    }
+
+    super.afterAll()
+  }
+
+}
