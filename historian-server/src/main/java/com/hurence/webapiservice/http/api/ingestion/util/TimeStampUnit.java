fcffb98f39ee7317b3c4d3b79bf3657cf62eff47
FeizNouri <38252619+FeizNouri@users.noreply.github.com>
2020-06-05 08:56:17 +0100

    Fix the failed points counting bugs in import csv and redesign code (#110)
    * Fixed a bug in import csv. Improved code readibility.
    
    Co-authored-by: Séguin-Henry Grégoire <gregoire.seguin-henry@hurence.com>

---
+++ historian-server/src/main/java/com/hurence/webapiservice/http/api/ingestion/util/TimeStampUnit.java
@@ -0,0 +1,8 @@
+package com.hurence.webapiservice.http.api.ingestion.util;
+
+public class TimeStampUnit {
+    public final static String MILLISECONDS_EPOCH = "MILLISECONDS_EPOCH";
+    public final static String SECONDS_EPOCH = "SECONDS_EPOCH";
+    public final static String MICROSECONDS_EPOCH = "MICROSECONDS_EPOCH";
+    public final static String NANOSECONDS_EPOCH = "NANOSECONDS_EPOCH";
+}
