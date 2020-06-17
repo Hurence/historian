b90e4b77114d63cc7ccf74376cef12f823fe0c5c
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2020-06-17 14:51:20 +0200

    Renamed a variable.

--- historian-server/src/main/java/com/hurence/webapiservice/http/api/grafana/parser/SearchValuesRequestParser.java
+++ historian-server/src/main/java/com/hurence/webapiservice/http/api/grafana/parser/SearchValuesRequestParser.java
@@ -1,6 +1,5 @@
 package com.hurence.webapiservice.http.api.grafana.parser;
 
-import com.hurence.webapiservice.http.api.grafana.modele.QueryRequestParam;
 import com.hurence.webapiservice.http.api.grafana.modele.SearchValuesRequestParam;
 import io.vertx.core.json.JsonObject;
 import org.slf4j.Logger;
@@ -9,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class SearchValuesRequestParser {
     private static final Logger LOGGER = LoggerFactory.getLogger(SearchRequestParser.class);
 
-    public static final int DEFAULT_MAX_DATAPOINTS = 1000;
+    public static final int DEFAULT_MAX_NUMBER_OF_VALUES_TO_RETURN = 1000;
 
     private final String field;
     private final String query;
@@ -31,7 +30,7 @@ public class SearchValuesRequestParser {
         builder.withField(fieldToSearch);
         builder.withQuery(queryToUseInSearch);
         Integer maxNumberOfMetricNameToReturn = parseMaxNumberOfMetricToReturn(requestBody);;
-        builder.withMaxNumberOfMetricToReturn(maxNumberOfMetricNameToReturn == null ? DEFAULT_MAX_DATAPOINTS : maxNumberOfMetricNameToReturn);
+        builder.withMaxNumberOfMetricToReturn(maxNumberOfMetricNameToReturn == null ? DEFAULT_MAX_NUMBER_OF_VALUES_TO_RETURN : maxNumberOfMetricNameToReturn);
         return builder.build();
     }
 
