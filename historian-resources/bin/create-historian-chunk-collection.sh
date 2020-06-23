56e0c9a4b8734592d5a307af49d21eed49e807ba
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2020-06-23 08:35:59 +0200

    Fixed install script

--- historian-resources/bin/create-historian-chunk-collection.sh
+++ historian-resources/bin/create-historian-chunk-collection.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-decalre -r SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source SCRIPT_DIR/solr-util.sh
+declare -r SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$SCRIPT_DIR"
+source solr-util.sh
 
 ################################
 # GLOBAL VARIABLES
