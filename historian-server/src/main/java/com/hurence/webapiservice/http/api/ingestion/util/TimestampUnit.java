2589f1f7945c62f296256b55c985df2b519287e8
FeizNouri <feiznouri@gmail.com>
2020-06-26 09:37:01 +0100

    adding TimestampUnit enum

---
+++ historian-server/src/main/java/com/hurence/webapiservice/http/api/ingestion/util/TimestampUnit.java
@@ -0,0 +1,8 @@
+package com.hurence.webapiservice.http.api.ingestion.util;
+
+public enum TimestampUnit {
+    MILLISECONDS_EPOCH,
+    SECONDS_EPOCH,
+    MICROSECONDS_EPOCH,
+    NANOSECONDS_EPOCH
+}
