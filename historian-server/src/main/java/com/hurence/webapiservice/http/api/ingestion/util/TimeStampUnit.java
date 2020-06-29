657fcb22ff336a4a372f384d0e77f5e78c8c8942
bastiengithub <bastien.wichroff@gmail.com>
2020-06-29 09:58:29 +0200

    Revert "TimestampUnit"
    This reverts commit 120f13e0

--- historian-server/src/main/java/com/hurence/webapiservice/http/api/ingestion/util/TimeStampUnit.java
+++ historian-server/src/main/java/com/hurence/webapiservice/http/api/ingestion/util/TimeStampUnit.java
@@ -1,8 +1,8 @@
 package com.hurence.webapiservice.http.api.ingestion.util;
 
-public enum TimestampUnit {
-    MILLISECONDS_EPOCH,
-    SECONDS_EPOCH,
-    MICROSECONDS_EPOCH,
-    NANOSECONDS_EPOCH
+public class TimeStampUnit {
+    public final static String MILLISECONDS_EPOCH = "MILLISECONDS_EPOCH";
+    public final static String SECONDS_EPOCH = "SECONDS_EPOCH";
+    public final static String MICROSECONDS_EPOCH = "MICROSECONDS_EPOCH";
+    public final static String NANOSECONDS_EPOCH = "NANOSECONDS_EPOCH";
 }
