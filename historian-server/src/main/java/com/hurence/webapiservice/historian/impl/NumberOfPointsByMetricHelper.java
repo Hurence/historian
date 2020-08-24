28ad14bde936f70f478ba60dd602f2720930973d
FeizNouri <feiznouri@gmail.com>
2020-08-24 17:06:56 +0100

    add a java doc

--- historian-server/src/main/java/com/hurence/webapiservice/historian/impl/NumberOfPointsByMetricHelper.java
+++ historian-server/src/main/java/com/hurence/webapiservice/historian/impl/NumberOfPointsByMetricHelper.java
@@ -2,5 +2,10 @@ package com.hurence.webapiservice.historian.impl;
 
 public interface NumberOfPointsByMetricHelper {
 
+    /**
+     *  used to get stream expression to use depending on if quality matters or not.
+     *
+     * @return StringBuilder to be used in the solr Stream query
+     */
     StringBuilder getStreamExpression();
 }
