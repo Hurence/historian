349933f7aab1f9062f3d898f06e8b68833118bb5
Gregoire Seguin-Henry <gregoire.seguin-henry@hurence.com>
2020-09-07 15:31:20 +0200

    The path should be an absolute path. Not using string when using cd.

--- historian-resources/bin/install.sh
+++ historian-resources/bin/install.sh
@@ -126,7 +126,7 @@ print_conf() {
   echo "SOLR_HOST_PORT_SOLR : $SOLR_HOST_PORT_SOLR"
   echo "CHUNK_COLLECTION_NAME : $CHUNK_COLLECTION_NAME"
   echo "REPORT_COLLECTION_NAME : $REPORT_COLLECTION_NAME"
-  echo "TAG_NAMES to add for time series : ${TAG_NAMES[*]}"
+  echo "TAG_NAMES to add for time series : $(join_by , "${TAG_NAMES[@]}")"
   echo "USING_EMBEDDED_GRAFANA : $USING_EMBEDDED_GRAFANA"
   echo "INSTALLING_DATASOURCE_PLUGIN : $INSTALLING_DATASOURCE_PLUGIN"
   echo "GRAFANA_PLUGIN_DIR : $GRAFANA_PLUGIN_DIR"
@@ -149,6 +149,32 @@ ask_and_set_variable() {
   export "${variable_name_to_modify}=${variable_value:-$DEFAULT_VALUE}"
 }
 
+#ask user to enter value for the variable, if user just press enter the default value is used instead
+#param1 Variable name to set
+#param2 Default value for the variable
+#param3 Msg description
+ask_and_set_variable_should_be_absolute_path() {
+  local MSG="$3"
+  local variable_name_to_modify="$1"
+  local -n variable_value="${variable_name_to_modify}"
+  local DEFAULT_VALUE="$2"
+  echo "${MSG}[$DEFAULT_VALUE]"
+  read -r "${variable_name_to_modify?}"
+  export "${variable_name_to_modify}=${variable_value:-$DEFAULT_VALUE}"
+  if [[ ! ${variable_value:-$DEFAULT_VALUE} = /* ]]; then
+        echo "Not an acceptable value, please specify an absolute path."
+        ask_and_set_variable_should_be_absolute_path "$1" "$2" "$3"
+  fi 
+}
+
+join_by() { 
+  local d=$1; 
+  shift; 
+  local f=$1; 
+  shift; 
+  printf %s "$f" "${@/#/$d}"; 
+  echo
+}
 #Similar to ask_and_set_variable but the input variable is an array and user can enter several value
 #The variable array must previusly exist ! Indeed it is not possible to export an array variable
 #param1 Variable name to set
@@ -172,7 +198,8 @@ ask_and_update_array() {
           _array+=("$tag_name")
         fi
       done
-      echo Tags :[${_array[*]}]
+      echo "Tags :"
+      join_by , "${_array[@]}"
       CREATE=true
       ;;
 
@@ -222,15 +249,15 @@ ask_and_set_boolean_variable() {
 
 add_tag_names_to_chunk_collection() {
   for tag in "${TAG_NAMES[@]}"; do
-    "$HISTORIAN_HOME/bin/modify-collection-schema.sh" -c "$CHUNK_COLLECTION_NAME" -s "$SOLR_HOST_PORT_SOLR" -f "$tag"
+    $HISTORIAN_HOME/bin/modify-collection-schema.sh -c "$CHUNK_COLLECTION_NAME" -s "$SOLR_HOST_PORT_SOLR" -f "$tag"
     echo -e "\n"
   done
 }
 
 download_and_extract_historian_into_hdh_home() {
   #Historian download & extract
-  wget https://github.com/Hurence/historian/releases/download/v1.3.5/historian-1.3.5-bin.tgz
-  mkdir -p "$HDH_HOME" && tar -xf historian-*-bin.tgz -C "$HDH_HOME"
+  wget https://github.com/Hurence/historian/releases/download/v1.3.5/historian-1.3.5-bin.tgz  
+  mkdir -p $HDH_HOME && tar -xf historian-*-bin.tgz -C $HDH_HOME
   rm historian-*-bin.tgz
   echo "installed historian into $HDH_HOME"
 }
@@ -241,12 +268,12 @@ install_embedded_solr_and_start_it_if_needed() {
     # create 2 data folders for SolR data
     local -r SOLR_NODE_1="$HDH_HOME/data/solr/node1"
     local -r SOLR_NODE_2="$HDH_HOME/data/solr/node2"
-    mkdir -p "$SOLR_NODE_1" "$SOLR_NODE_2"
+    mkdir -p $SOLR_NODE_1 $SOLR_NODE_2
     # touch a few config files for SolR
     local -r SOLR_XML_PATH="$HISTORIAN_HOME/conf/solr.xml"
-    cp "${SOLR_XML_PATH}" "${SOLR_NODE_1}/solr.xml"
-    cp "${SOLR_XML_PATH}" "${SOLR_NODE_2}/solr.xml"
-    touch "${HDH_HOME}/data/solr/node1/zoo.cfg"
+    cp ${SOLR_XML_PATH} ${SOLR_NODE_1}/solr.xml
+    cp ${SOLR_XML_PATH} ${SOLR_NODE_2}/solr.xml
+    touch ${HDH_HOME}/data/solr/node1/zoo.cfg
     # get SolR 8.2.0 and unpack it
     wget https://archive.apache.org/dist/lucene/solr/8.2.0/solr-8.2.0.tgz
     tar -xf solr-8.2.0.tgz
@@ -255,10 +282,10 @@ install_embedded_solr_and_start_it_if_needed() {
     local -r SOLR_HOME="$HDH_HOME/solr-8.2.0"
     # démarre un core Solr localement ainsi qu'un serveur zookeeper standalone.
     # We use force to enable solr to be run even as root user.
-    "${SOLR_HOME}/bin/solr" start -cloud -s "$SOLR_NODE_1" -p 8983 -force
+    ${SOLR_HOME}/bin/solr start -cloud -s $SOLR_NODE_1 -p 8983 -force
     # démarre un second core Solr localement qui va utiliser le serveur zookeeper précédament créer.
     # We use force to enable solr to be run even as root user.
-    "${SOLR_HOME}/bin/solr" start -cloud -s "$SOLR_NODE_2" -p 7574 -z localhost:9983 -force
+    ${SOLR_HOME}/bin/solr start -cloud -s $SOLR_NODE_2 -p 7574 -z localhost:9983 -force
     echo "solr is now running at ${SOLR_HOST_PORT_SOLR}"
     echo -e "\n"
   fi
@@ -285,8 +312,8 @@ intall_grafana_if_asked_for_linux() {
 intall_grafana_datasource_plugin_if_asked() {
   if [[ $INSTALLING_DATASOURCE_PLUGIN = true ]]; then
     wget https://github.com/Hurence/grafana-historian-datasource/archive/v1.0.0.tar.gz
-    mkdir -p "$GRAFANA_PLUGIN_DIR"
-    tar -zxf v1.0.0.tar.gz -C "$GRAFANA_PLUGIN_DIR"
+    mkdir -p $GRAFANA_PLUGIN_DIR
+    tar -zxf v1.0.0.tar.gz -C $GRAFANA_PLUGIN_DIR
     rm v1.0.0.tar.gz
     echo "Installed grafana plugin into $GRAFANA_PLUGIN_DIR"
     echo -e "\n"
@@ -295,7 +322,7 @@ intall_grafana_datasource_plugin_if_asked() {
 
 start_grafana_if_asked() {
   if [[ $USING_EMBEDDED_GRAFANA = true ]]; then
-    cd "$GRAFANA_HOME"
+    cd $GRAFANA_HOME
     nohup ./bin/grafana-server web &
     cd -
   fi
@@ -348,24 +375,38 @@ generate_historian_server_conf() {
         "socket_timeout": 60000
       }
     }
-  }' >"$HISTORIAN_HOME/conf/historian-server-conf.json"
+  }' > $HISTORIAN_HOME/conf/historian-server-conf.json
 }
 
 create_historian_collections() {
   # create collection in SolR
-  "$HISTORIAN_HOME/bin/create-historian-chunk-collection.sh" -c "$CHUNK_COLLECTION_NAME" -s "$SOLR_HOST_PORT_SOLR"
+  $HISTORIAN_HOME/bin/create-historian-chunk-collection.sh -c "$CHUNK_COLLECTION_NAME" -s "$SOLR_HOST_PORT_SOLR"
   echo -e "\n"
   # create report collection in SolR
-  "$HISTORIAN_HOME/bin/create-historian-report-collection.sh" -c "$REPORT_COLLECTION_NAME" -s "$SOLR_HOST_PORT_SOLR"
+  $HISTORIAN_HOME/bin/create-historian-report-collection.sh -c "$REPORT_COLLECTION_NAME" -s "$SOLR_HOST_PORT_SOLR"
   echo -e "\n"
 }
 
 start_historian_server() {
   # and launch the historian REST server
   echo "Install completed. Starting historian..."
-  "$HISTORIAN_HOME/bin/historian-server.sh" start
+  $HISTORIAN_HOME/bin/historian-server.sh start
   echo "The historian server is now running."
   echo "You can use ./bin/historian-server.sh [start|stop|restart] to manage the historian server."
+}
+
+go_to_home_directory() {
+  cd ${HDH_HOME} || (echo "could not go to ${HDH_HOME} folder" && exit 1)
+}
+
+
+create_home_directory() {
+  mkdir -p ${HDH_HOME}
+  CREATION_OF_HDH_HOME_RESULT=$?
+  if [[ $CREATION_OF_HDH_HOME_RESULT != 0 ]]; then
+    echo "could not create $HDH_HOME folder ! returned $CREATION_OF_HDH_HOME_RESULT"
+    exit $CREATION_OF_HDH_HOME_RESULT
+  fi
 }
 
 main() {
@@ -376,10 +417,11 @@ main() {
       declare -a TAG_NAMES=()
       setup_all_variables
       print_conf
-      #    Start the installation
-      mkdir -p "$HDH_HOME"
+      create_home_directory
+      go_to_home_directory
+      echo "HDH_HOME  is $HDH_HOME"
+      echo "directory of installation is $(pwd)"
       download_and_extract_historian_into_hdh_home
-      cd "$HDH_HOME" || (echo "could not go to $HDH_HOME folder" && exit 1)
       install_embedded_solr_and_start_it_if_needed
       create_historian_collections
       add_tag_names_to_chunk_collection
@@ -397,10 +439,11 @@ main() {
       declare -a TAG_NAMES=()
       setup_all_variables
       print_conf
-      #    Start the installation
-      mkdir -p "$HDH_HOME"
+      create_home_directory
+      go_to_home_directory
+      echo "HDH_HOME  is $HDH_HOME"
+      echo "directory of installation is $(pwd)"
       download_and_extract_historian_into_hdh_home
-      cd "$HDH_HOME" || (echo "could not go to $HDH_HOME folder" && exit 1)
       install_embedded_solr_and_start_it_if_needed
       create_historian_collections
       add_tag_names_to_chunk_collection
