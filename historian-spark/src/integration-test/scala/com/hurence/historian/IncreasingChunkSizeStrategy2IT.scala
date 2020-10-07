c65d6721e228146ad3a64b8d76a8ea0dded0cc79
oalam <bailet.thomas@gmail.com>
2020-10-07 18:13:09 +0200

    some files forgotten

---
+++ historian-spark/src/integration-test/scala/com/hurence/historian/IncreasingChunkSizeStrategy2IT.scala
@@ -0,0 +1,30 @@
+//package com.hurence.historian
+//
+//import com.hurence.historian.spark.compactor.{ChunkCompactorConfStrategy2, ChunkCompactorJobStrategy2SchemaVersion0}
+//import com.hurence.historian.spark.compactor.job.CompactorJobReport
+//import com.hurence.logisland.record.TimeSeriesRecord
+//import io.vertx.core.json.JsonObject
+//import org.junit.jupiter.api.Assertions.assertEquals
+//import org.testcontainers.containers.DockerComposeContainer
+//
+//class IncreasingChunkSizeStrategy2IT(container: (DockerComposeContainer[SELF]) forSome {type SELF <: DockerComposeContainer[SELF]})
+//  extends AbstractIncreasingChunkSizeIT(container) {
+//
+//  val compactorConf: ChunkCompactorConfStrategy2 = ChunkCompactorConfStrategy2(zkUrl, historianCollection,
+//    CompactorJobReport.DEFAULT_COLLECTION,
+//    chunkSize = 10,
+//    saxAlphabetSize = 2,
+//    saxStringLength = 3,
+//    solrFq = s"${TimeSeriesRecord.CHUNK_ORIGIN}:logisland",
+//    false,
+//    true
+//  )
+//
+//  override def createCompactor() = {
+//    new ChunkCompactorJobStrategy2SchemaVersion0(compactorConf)
+//  }
+//
+//  override def additionalTestsOnReportEnd(report: JsonObject): Unit = {
+//    assertEquals(compactorConf.toJsonStr, report.getString(CompactorJobReport.JOB_CONF))
+//  }
+//}
