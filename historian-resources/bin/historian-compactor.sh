56c3a8652303ea434834c50e33b53949f7d3d13f
Mathieu Rossignol <mathieu.rossignol@hurence.com>
2020-10-29 17:43:07 +0100

    Started to create script/env variable system for running compactor. Not finished

--- historian-resources/bin/historian-compactor.sh
+++ historian-resources/bin/historian-compactor.sh
@@ -1,209 +1,148 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# TODO port this script to run compactor (currently a copy paste from historian server run script)
+################################################################################
+# Set needed directories and default variables
+################################################################################
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ROOT_DIR="$DIR/.."
-PID=$ROOT_DIR/app.pid
-LOG=$ROOT_DIR/app.log
-ERROR=$ROOT_DIR/app-error.log
-DEBUG_MODE=false
+CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+HISTORIAN_HOME="$( cd "${CURRENT_SCRIPT_DIR}/.." >/dev/null 2>&1 && pwd )"
+HISTORIAN_LIB_DIR="${HISTORIAN_HOME}/lib"
+HISTORIAN_CONF_DIR="${HISTORIAN_HOME}/conf"
 
-USR=user
+# Default configuration file path
+HISTORIAN_CONFIG_FILE=${HISTORIAN_CONF_DIR}/historian-compactor.yaml
+# Default environment variables file
+HISTORIAN_VARS_FILE=${HISTORIAN_CONF_DIR}/historian-compactor-envs
 
-print_usage(){
+################################################################################
+# Functions
+################################################################################
+
+# Prints usage
+usage() {
+    SCRIPT_NAME=$(basename ${0})
     cat << EOF
 
-bash historian-compactor.sh [options]
+--------------------------------------------------------------------------------
+Command to manage the historian compactor spark job. Usage:
 
+${SCRIPT_NAME} <command> [options]
+
+<command>:
+            help: Print this help then exits.
+            start: Start the historian compactor job.
 [options]:
+            -c|--config <config-file-path> : Use configuration file different
+              from the default one (${HISTORIAN_CONFIG_FILE}).
+            -n|--no-var-file : Do not use any environment variables file (which
+              defaults to ${HISTORIAN_VARS_FILE}).
+            -v|--var-file <var-file-path> : Use the provided environment
+              variables file instead of the default one (${HISTORIAN_VARS_FILE}).
+            -s|--spark-home <spark-home-path> : The path to spark home for
+              finding the spark-submit command. If not set, will use the
+              SPARK_HOME environment variable. This creates or overwrites the
+              SPARK_HOME environment variable.
+            -h|--hadoop-config <hadoop-config-path> : The path to the directory
+              where the core-site.xml file path resides. If not set, will use
+              the HADOOP_CONF_DIR environment variable. If the yarn-site.xml
+              file is in the same location as the core-site.xml file then it
+              will be used. Otherwise you have to set the YARN_CONF_DIR
+              environment variable so that spark-submit knows how to contact the
+              YARN resource manager server. This creates or overwrites the
+              HADOOP_CONF_DIR environment variable.
+            -y|--yarn-config <yarn-config-path> : The path to the directory
+              where the yarn-site.xml file path resides. May be useless if
+              yarn-site.xml file is already in <hadoop-config-path> or the
+              YARN_CONF_DIR environment variable is set. This creates or
+              overwrites the YARN_CONF_DIR environment variable.
+            -krb|--kerberos : Enable kerberos authentication. If used,
+              principal and keytab options must be set or the corresponding
+              environment variables.
+            -p|--principal <principal>: Use kerberos with the passed principal.
+              Must be used in conjunction with the -k option. This creates or
+              overwrites the KERBEROS_PRINCIPAL environment variable.
+            -kt|--keytab <keytab-file-path>: Use kerberos with the passed keytab
+              file path. Must be used in conjunction with the -p option. This
+              creates or overwrites the KERBEROS_KEYTAB environment variable.
 
-             start
-             stop
-             restart
-             status
+Examples:
 
--d|--debug if you want debug level logs.
-
-by Hurence, 2020
-
-The script start or stop the historian compactor.
+${SCRIPT_NAME} start
+${SCRIPT_NAME} start -c /foobar/custom-historian-compactor.yaml
+--------------------------------------------------------------------------------
 
 EOF
 }
 
-# Parse method is not used for now but we put one exemple here if we want to add arguments
-parse_args() {
-    POSITIONAL=()
-    while [[ $# -gt 0 ]]
+# Prints usage then exits with error
+# shellcheck disable=SC2120
+print_usage_and_exit_on_error() {
+  usage
+  EXIT_CODE=1
+  if [ $# -ne 0 ]
+  then
+    # Exit code passed, use it
+    EXIT_CODE=${1}
+  fi
+  exit "${EXIT_CODE}"
+}
+# Parse options given by user
+parse_input() {
+  #echo "Processing options : $*"
+
+  # Check there is at least one parameter
+  if [[ $# -eq 0 ]]
+  then
+    # shellcheck disable=SC2119
+    echo "Missing command"
+    print_usage_and_exit_on_error
+  fi
+
+  while [[ $# -gt 0 ]]
     do
-        key="$1"
+      param="$1"
 
-        case $key in
-            -d|--debug)
-                DEBUG_MODE=true
-                shift # past argument
-            ;;
-            -h|--help)
-                print_usage
-                exit 0
-            ;;
-            *)  # unknown option
-                POSITIONAL+=("$1") # save it in an array for later
-                shift # past argument
-            ;;
-        esac
-    done
-
-    if [[ $DEBUG_MODE = true ]]
-    then
-      echo "parsing command line args"
-      echo "DEBUG_MODE is set to '${DEBUG_MODE}'";
-      echo "DIR is $DIR"
-      echo "ROOT_DIR is $ROOT_DIR"
-    fi
-
-    set -- "${POSITIONAL[@]}" # restore positional parameters
-
-}
-
-status() {
-    echo
-    echo "==== Status"
-
-    if [ -f $PID ]
-    then
-        echo
-        echo "Pid file: $( cat $PID ) [$PID]"
-        echo
-        ps -ef | grep -v grep | grep $( cat $PID )
-    else
-        echo
-        echo "No Pid file"
-    fi
-}
-
-start_historian() {
-  echo "==== Start"
-  touch "$PID"
-#       Using an array to stock command hinder double interpretation of args
-#       see https://unix.stackexchange.com/questions/444946/how-can-we-run-a-command-stored-in-a-variable for more info
-  COMMAND=(java)
-  if [[ $DEBUG_MODE = true ]]
-  then
-    COMMAND+=("-Dlog4j.configuration=file:$ROOT_DIR/conf/log4j-debug.properties")
-    COMMAND+=("-Dlog4j.configurationFile=file:$ROOT_DIR/conf/log4j-debug.properties")
-  else
-    COMMAND+=("-Dlog4j.configuration=file:$ROOT_DIR/conf/log4j.properties")
-    COMMAND+=("-Dlog4j.configurationFile=file:$ROOT_DIR/conf/log4j.properties")
-  fi
-  COMMAND+=(-jar "$ROOT_DIR/lib/historian-server-1.3.5-fat.jar" --conf "$ROOT_DIR/conf/historian-server-conf.json")
-  if [[ $DEBUG_MODE = true ]]
-  then
-    echo "run below command"
-    echo "nohup ${COMMAND[@]} >> ${LOG} 2>&1 &"
-  fi
-  if nohup "${COMMAND[@]}" >> "$LOG" 2>&1 &
-  then echo $! > "$PID"
-       echo -e "${GREEN}Started.${NOCOLOR}"
-       echo -e "${GREEN}check logs at '$LOG'${NOCOLOR}"
-       echo "$(date '+%Y-%m-%d %X'): START" >> "$LOG"
-  else echo "Error... "
-       /bin/rm "$PID"
-  fi
-}
-
-start() {
-    if [ -f "$PID" ]
-    then
-#                        check if really exist
-        pid_in_file=$(cat $PID )
-        kill -0 $pid_in_file
-        if [[ $? = 0 ]];then
-#          find historian-server-processes
-          historian_process=$(ps u --pid $pid_in_file)
-          if [[ "$historian_process" == *historian-server-1.3.5-fat.jar* ]];then
-            echo
-            echo "Already started. PID: [$( cat $PID )]"
-          else
-            echo "cleaning not empty PID file. (processus not running anymore)."
-            rm $PID
-            start_historian
+      case ${param} in
+        # Commands
+        start)
+          COMMAND="start"
+        ;;
+        help)
+            usage
+            exit 0
+        ;;
+        # Options
+        -c|--config)
+          if [ "X${2}" == "X" ]
+          then
+            echo "Missing configuration file after ${param} option"
+            print_usage_and_exit_on_error
           fi
-        else
-          echo "cleaning not empty PID file. (processus not running anymore)."
-          rm $PID
-          start_historian
-        fi
-    else
-        start_historian
-    fi
-}
-
-kill_cmd() {
-    SIGNAL=""; MSG="Killing "
-    while true
-    do
-        LIST=`ps -ef | grep -v grep | grep $CMD | grep -w $USR | awk '{print $2}'`
-        if [ "$LIST" ]
-        then
-            echo; echo "$MSG $LIST" ; echo
-            echo $LIST | xargs kill $SIGNAL
-            sleep 2
-            SIGNAL="-9" ; MSG="Killing $SIGNAL"
-            if [ -f $PID ]
-            then
-                /bin/rm $PID
-            fi
-        else
-           echo; echo "All killed..." ; echo
-           break
-        fi
+          HISTORIAN_CONFIG_FILE="${2}"
+          shift # Next argument
+        ;;
+        # Rest
+        *)
+          # Unknown parameter
+          echo "Unknown command or option: ${param}"
+          print_usage_and_exit_on_error
+        ;;
+      esac
+      shift # Next argument
     done
 }
 
-stop() {
-    echo "==== Stop"
-    if [ -f $PID ]
-    then
-        if kill $( cat $PID )
-        then echo -e "${GREEN}Stopped. ${NOCOLOR}"
-             echo "$(date '+%Y-%m-%d %X'): STOP" >>$LOG
-        fi
-        rm $PID
-        kill_cmd
-    else
-        echo "No pid file. Already stopped?"
-    fi
-}
+################################################################################
+# Main
+################################################################################
 
-main() {
-  parse_args "$@"
-  case "$1" in
-    'start')
-            start
-            ;;
-    'stop')
-            stop
-            ;;
-    'restart')
-            stop ; echo "Sleeping..."; sleep 3 ;
-            start
-            ;;
-    'status')
-            status
-            ;;
-    *)
-            echo
-            echo "Usage: $0 { start | stop | restart | status }"
-            echo
-            exit 1
-            ;;
-  esac
-}
+echo ${HISTORIAN_HOME}
+echo ${HISTORIAN_LIB_DIR}
+echo ${HISTORIAN_CONF_DIR}
 
-main "$@"
-exit 0
+# Parse options
+echo
+parse_input "$@"
 
-
-
+echo "Command: ${COMMAND}"
+echo "Configuration file: ${HISTORIAN_CONFIG_FILE}"
